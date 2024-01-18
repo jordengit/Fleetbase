27253800b799866126d78622cd3be4e781bbf060
DesignComb <designcomb2021@gmailcom>
2024-01-18 13:16:54 +0800

    chore: remove packages

--- packages/ember-core/addon/utils/load-extensions.js
+++
@@ -1,8 +0,0 @@
-export default async function loadExtensions() {
-    return new Promise((resolve, reject) => {
-        return fetch('/extensions.json')
-            .then((resp) => resp.json())
-            .then(resolve)
-            .catch(reject);
-    });
-}
